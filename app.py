1514e14a70bf63793b84fa070681f662a6d45f78
Astarter <gbh7788280758@126.com>
2021-03-22 01:39:58 -0700

    Update app.py
    start the building of layout

--- app.py
+++ app.py
@@ -8,11 +8,31 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
-
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container(
-    html.H1("GDP")
-)
+    children=[
+        dbc.NavbarSimple(
+            children=[
+                dbc.DropdownMenu(
+                    children=[
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                    ],
+                    nav=True,
+                    in_navbar=True,
+                    label="References",
+                ),
+            ],
+            brand="NavbarSimple",
+            brand_href="#",
+            color="primary",
+            dark=True,
+        ),
+    ], style={"width": "85%"})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
